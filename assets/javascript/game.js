cb0e6683f21ad0dc2b5b9f2d485db612984a6754
tbsteeze <trevbrow@live.com>
2019-07-18 05:14:45 -0600

    for loop to create list items for each letter in comp guess selection, need to figure out how to add to ordered list

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,37 +2,49 @@
     var game = {
         wins: 0,
         losses: 0,
-        mtnNames:["timponogos", "olympus", "jupiter", "sunset", "superior", "wolverine"],
+        mtnNames:["timponogos", "olympus", "jupiter", "sunset", "superior", "wolverine", "lone peak", "gobblers knob"],
         compGuess:[],
         correctGuess:[],
         incorrectGuess:[],
+        
     
     }
 
-  //Set Computer Guess to select a peak name within array of mountain names based on math.ramdom function   
+    //Set Computer Guess to select a peak name within array of mountain names based on math.ramdom function   
     compGuess = game.mtnNames[Math.floor(Math.random()*game.mtnNames.length)];
     console.log(compGuess);
 
-    for (let i = 0; i < compGuess.length; i++) {
-        
-        
-    }
+    //Creates empty list within #word-container to place a series of list elements corresponding with the total number of letters in the word
     
-    // console.log(game.mtnNames.length);
+    var compGuessList = document.getElementById("word-container");
     
-    // var userGuess = document
+    
+    //Creates a placeholder of _ for each letter within compGuess; make sure to change <li> to inline element to display on same line in css
+    function createListItem (text) {
+        for (let i = 0; i < compGuess.length; i++){ 
+            var letter = document.createElement("li");
+            var placeholder = document.createTextNode("_");
+            letter.appendChild(placeholder);
+            console.log(letter);
+    }}
+    // function addListItem (parent, children) {
+    //     children.
+    // }
+
+    // }
+    
+    
+  
+    
+    
 //Upon key stroke, select random number to key to array of possible words
 
 
 // Take the indexed value within the array and create a new string variable
-    // document.onkeyup 
-    
-    // var mtn = 
+
 
 // Split mountain name into an array of its letters
-    // for (let i = 0; i < mtnName.length; i++) {
-    //     console.log(mtnName.charAt(i));
-        
-    // }
 
+        
+   
 //
