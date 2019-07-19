7e75e6c0e02a719bbdb7e29f003c45bd19de99e6
tbsteeze <trevbrow@live.com>
2019-07-19 14:57:20 -0600

    trying to relocate begining function to game object, having difficulty initiating game and recalling code to respond to onkeypress

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,37 +1,54 @@
 //Display blank game, containing letters the length of the word in underscore blank spaces, and "play any key to start" on screen, placeholder image in left box, game default screen on right, initialize all variables to empty arrays or 0, create variables and functions within object
+    //Game object containing methods
     var game = {
         wins: 0,
         losses: 0,
+        guessRemaining: 12,
         mtnNames:["timponogos", "olympus", "jupiter", "sunset", "superior", "wolverine", "lone peak", "gobblers knob"],
         compGuess:[],
+        userGuess: [],
         correctGuess:[],
         incorrectGuess:[],
+        isPlaying: false,
         
+        //Set Computer Guess to select a peak name within array of mountain names based on math.ramdom function   
+        selectWord: function() {
+        compGuess = game.mtnNames[Math.floor(Math.random()*game.mtnNames.length)];
+        console.log(game.compGuess);
+        },
+        //Creates empty list within #word-container to place a series of list elements containing "_ "corresponding with the total number of letters in the word
+        
+
+        guessCount: function() {
+            guessRemaining = guessRemaining - 1;
+        }
         
                 
     }
-
-    //Set Computer Guess to select a peak name within array of mountain names based on math.ramdom function   
-    game.compGuess = game.mtnNames[Math.floor(Math.random()*game.mtnNames.length)];
-    console.log(game.compGuess);
-
-    //Creates empty list within #word-container to place a series of list elements containing "_ "corresponding with the total number of letters in the word
-    
-    for (let i = 0; i < game.compGuess.length; i++) { 
-        var letter = document.createElement("li");
-        var placeholder = document.createTextNode("_ ");
-        letter.appendChild(placeholder);
-        document.getElementById("word-container").appendChild(letter);
-             
+//This code works on refresh, but having difficulty setting to onkeypress
+     function beginGame() {
+        for (let i = 0; i < game.compGuess.length; i++) { 
+            var letter = document.createElement("li");
+            var placeholder = document.createTextNode("_ ");
+            letter.appendChild(placeholder);
+            document.getElementById("word-container").appendChild(letter);
+            isPlaying = true;
+        }
     }
+
     
     
+       
     
     //Runs through code and checks userGuess vs individual letters in computerGuess
-    // document.onkeypress(event.keyCode {
+    document.onkeypress = function(event) {
+        game.selectWord();
+        game.beginGame();
+        document.getElementById("begin").textContent = "press a key, take a guess";
+        document.getElementById("guess-remaining") = 
+        console.log(game.mtnNames);
+    }
 
-    // })
-    
     
  
  
