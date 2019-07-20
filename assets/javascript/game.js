6a40cfe51953a804b3b927a7fd4b8578d1cf62ab
tbsteeze <trevbrow@live.com>
2019-07-20 07:58:53 -0600

    trying to figure out how to repalce each individual list item with its correct guessed letter

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,28 +12,32 @@
         isPlaying: false,
         
         //Set Computer Guess to select a peak name within array of mountain names based on math.ramdom function   
-        selectWord: function() {
-        compGuess = game.mtnNames[Math.floor(Math.random()*game.mtnNames.length)];
-        console.log(game.compGuess);
-        },
+        
         //Creates empty list within #word-container to place a series of list elements containing "_ "corresponding with the total number of letters in the word
         
 
-        guessCount: function() {
-            guessRemaining = guessRemaining - 1;
-        }
+        
         
                 
     }
 //This code works on refresh, but having difficulty setting to onkeypress
-     function beginGame() {
+    function selectWord() {
+        game.compGuess = game.mtnNames[Math.floor(Math.random()*game.mtnNames.length)];
+        isPlaying = true;
+        console.log(game.compGuess);
+    }
+    function createListPlaceholder() {
         for (let i = 0; i < game.compGuess.length; i++) { 
-            var letter = document.createElement("li");
+            var letter = document.createElement("li");//need something here to assign id to each individual list item
             var placeholder = document.createTextNode("_ ");
             letter.appendChild(placeholder);
             document.getElementById("word-container").appendChild(letter);
             isPlaying = true;
         }
+    }
+    function guessCount() {
+        game.guessRemaining = game.guessRemaining - 1;
+        return  game.guessRemaining;
     }
 
     
@@ -42,12 +46,28 @@
     
     //Runs through code and checks userGuess vs individual letters in computerGuess
     document.onkeypress = function(event) {
-        game.selectWord();
-        game.beginGame();
+        selectWord(game.mtnNames);
+        createListPlaceholder(game.compGuess);
+        guessCount(game.guessRemaining);
         document.getElementById("begin").textContent = "press a key, take a guess";
-        document.getElementById("guess-remaining") = 
-        console.log(game.mtnNames);
+        game.guessRemaining = document.getElementById("guess-remaining"); //does not work
+        game.isPlaying = true;
+        
+        var l = event.key.toLowerCase();
+        // For loop that runs through each letter of game.compguess to check to match l to a letter within the array  
+        for (let i = 0; i < game.compGuess.length; i++) {
+            if (l === game.compGuess[i])
+                
+        }
+            
+        // }
+
     }
+
+    
+    
+
+
 
     
  
