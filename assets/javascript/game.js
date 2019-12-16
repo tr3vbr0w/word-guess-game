fa83dd29c4878e4bfbca05666624c9809e003a85
tbsteeze <trevbrow@live.com>
2019-12-15 22:44:14 -0700

    game is now functioning, need to add and render images

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,10 +8,11 @@
     var mtnNames =["timponogos", "olympus", "jupiter", "sunset", "superior", "wolverine", "lone peak", "gobblers knob"]
     var compGuess = "";
     var compGuessArr = [];
+    var displayArr = [];
     var compGuessStr = [];
     var userGuess = "";
     var correctGuess = [];
-    var incorrectGuess = [0];
+    var incorrectGuess = [];
     var isPlaying = false;
     var winsDisplay = document.getElementById("numWins");
     var lossDisplay = document.getElementById("numLosses");
@@ -25,74 +26,53 @@
         compGuess = mtnNames[Math.floor(Math.random()*mtnNames.length)];
         isPlaying = true;
         compGuessArr = compGuess.split("");
-        // console.log(compGuessArr);
+        console.log('Comp guess array set here '+compGuessArr);
     }
-
+//Remove, empty or clear function for game reset
     function wrongGuess() {
         incorrectGuess.push(userGuess);
         var letter = document.createElement("li");
         var guess = document.createTextNode(userGuess + " ");
         letter.appendChild(guess);
         document.getElementById("guess-wrong").appendChild(letter);
+        guessRemaining--;
+
     }
 
     function createListPlaceholder() {
         for (let i = 0; i < compGuess.length; i++) { 
-            compGuessStr.push("_ ");
+            displayArr.push("_ ");
         }
-        // var letter = document.createElement("span");
-        // var placeholder = document.createTextNode(compGuessStr);
-        // console.log(compGuessStr);
-        // letter.appendChild(placeholder);
-        // document.getElementById("word-container").appendChild(letter);
+        compGuessStr = displayArr.join(" ");
         document.getElementById("word-container").textContent = compGuessStr;
-        
+        console.log('Display array set in createListPlaceholder'+ displayArr);
     }
-    function guessCount() {
-        guessRemaining = guessRemaining - 1;
-        guessR = guessRemaining.toString();
-        // console.log("Guesses remaining" + guessR);
-    }
-    
+   
     function matchCheck(userGuess) {
         if (compGuess.includes(userGuess)) {
             for (let i = 0; i < compGuess.length; i++) {
                 if (userGuess === compGuess[i]) {
-                    // console.log(i);
-                    compGuessStr.splice(i, 1, userGuess);
-                    
-
+                    displayArr.splice(i, 1, userGuess);
                 }
-
             }
-                // console.log(compGuessStr);
-
-
-                // compGuessArr[i].replace(compGuess[i]);
-                // console.log("Matching index " + i);
-                document.getElementById("word-container").textContent = compGuessStr;
-                // var outcome = true;
-                
+                document.getElementById("word-container").textContent = displayArr;
             } else {
-                var outcome = false;
                 wrongGuess();
-        
-            }       
-        // console.log(outcome);
+            }    
+        }
+
+    //win and loss count function
+    function gameReset() {
+        guessRemaining = 12;
+        isPlaying = false;
+        compGuessArr = [];
+        displayArr = [];
+        incorrectGuess = [];
+        document.getElementById("guess-wrong").innerHTML = "";
+        document.getElementById("word-container").innerHTML = "";
     }
 
-
-
-    
-    
-    //win and loss count function
-
     // User guess to list replacement, will mess with pending recognizing guest index matching
-    
- 
-    
-       
-    // look up nth child in js for the list items
     //Initializes game, generates placeholders for 
     document.onkeypress = function(event) { 
         userGuess = event.key;
@@ -110,21 +90,26 @@
     //     // For loop that runs through each letter of compguess to check to match l to a letter within the array
     //     //check index, split word
         } else if (isPlaying === true) {
-            matchCheck();
+            matchCheck(userGuess);
             
         
         }
+        // guessCount();
         if(guessRemaining === 0){
             alert("You Lose");
-            loss = loss + 1;
-            guessRemaining = 12;
-            isPlaying = false;
-            lossDisplay.textContent = "Losses: " + loss
+            loss++;
+            lossDisplay.textContent = "Losses: " + loss;
+            gameReset();
             
+        
+        } else if (displayArr.toString() === compGuessArr.toString()){
+            alert("You Win");
+            wins++;
+            winsDisplay.textContent = "Wins: " + wins;
+            gameReset();
         }
-        guessCount();
-        guessRemainDisplay.textContent = guessR; //does not work
-        winsDisplay.textContent = "Wins: " + wins
+        
+        guessRemainDisplay.textContent = guessRemaining; 
     }
 
             
@@ -145,18 +130,3 @@
   
     
     
-//Upon key stroke, select random number to key to array of possible words
-
-
-// Take the indexed value within the array and create a new string variable
-
-
-// Split mountain name into an array of its letters
-
-        
-   
-//compare key guess against work
-//win conditions / loss conditions
-//save already guessed letters
-//set parameters for user guess
-//increment wins or losses
